0a29a83539512b60ef66c13b009e3edf704c2e22
gimddanggu <changum0520@gmail.com>
2026-03-02 19:35:32 +0900

    Config 클래스 추가 (config.json 로드)

---
+++ app/include/util/Config.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+struct TcpConfig {
+    std::string ip;
+    int port;
+    int max_connections;
+    int timeout;
+};
+
+struct AppConfig {
+    TcpConfig tcp;
+};
+
+class Config {
+public:
+    static Config& getInstance();
+    bool load(const std::string& path);
+    const AppConfig& get() const;
+    
+private:
+    Config() = default;
+    AppConfig config_;
+};
