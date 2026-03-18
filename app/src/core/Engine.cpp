52730e6c324c089153227341bd2506b5f0636bfc
Owl-jun <80896230+Owl-jun@users.noreply.github.com>
2026-03-18 15:23:10 +0900

    main refactor

--- app/src/core/Engine.cpp
+++ app/src/core/Engine.cpp
@@ -4,5 +4,8 @@
 /* Protocol 등 Main Funtion 처리 */
 void Engine::Run()
 {
-
+    /* TCP 초기화 */
+    TcpServer server = TcpServer{};
+    /* UART 초기화 */
+    UART uart = UART{};
 }
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,11 +4,6 @@ int main(int argc, char* argv[])
 {   
     /* Main Function */
     Engine::Get().Run();
-
-    /* TCP 초기화 */
-    TcpServer server = TcpServer{};
-    /* UART 초기화 */
-    UART uart = UART{};
     
     /* Loop */
     while(1);
